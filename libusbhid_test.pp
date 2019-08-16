3ef6d222218c47978d1c70abeb1655df06c45dca
svpantazi <32182348+svpantazi@users.noreply.github.com>
2019-08-16 13:16:09 -0400

    Add files via upload

--- libusbhid_test.pp
+++ libusbhid_test.pp
@@ -2,7 +2,10 @@ program libusbhid_test;
 {
 S. V. Pantazi (svpantazi@gmail.com), 2013
 
-updated 08/13/2019  }
+updated
+	08/13/2019
+  08/16/2019
+}
 
 {$mode objfpc}{$H+}
 
@@ -15,73 +18,38 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
 
+  keyboard,
   sysutils,
-  libusbhid;
 
-const
-  REPORT_COUNT=1;
-  REPORT_LENGTH=128;
+  libusbhid,
 
-type
-  THIDDeviceReportData=array[0..REPORT_COUNT-1] of record
-    dataLen: Integer;
-    hid_data,
-    hid_data_copy:array[0..REPORT_LENGTH-1] of byte;
-  end;
-
-var
-  reportIdx,
-  i:integer;
-  device_context:libusbhid_context;
-  hidReportData:THIDDeviceReportData;
-  dataString:string;
-
-  procedure PrintAndUpdateReport(reportIdx:integer);
-  var
-    i:  Integer;
-  begin
-    dataString:='';
-
-    for i:=0 to hidReportData[reportIdx].dataLen-1 do
-    begin
-      if hidReportData[reportIdx].hid_data[i]=hidReportData[reportIdx].hid_data_copy[i] then
-      begin
-        Write('. ,');
-        dataString+='__';
-      end
-      else
-      begin
-        Write(Format('%0.2s,',[hexStr(hidReportData[reportIdx].hid_data[i],2)]));
-        dataString+=hexStr(hidReportData[reportIdx].hid_data[i],2);
-        hidReportData[reportIdx].hid_data_copy[i]:=hidReportData[reportIdx].hid_data[i];
-      end;
-    end;
-    WriteLn();
-  end;
-
+  hid_testing_utils;
 
 begin
   if libusbhid_open_device(
         // $046D, $C216 {xbox gamepad }
         // $0000, $0001 {barcode scanner}
-	      // $24C0, $0003 {}
+	      // $24C0, $0003 {weather station}
 	      // $1784, $0001 {}
         // $056a, $00de {Wacom Bamboo}
 	      $04e7, $0050  {elo touch screen}
-          																		,{instance=}1,device_context) then
+//        $051d,$0002 {APC UPS}
+
+				,{instance=}1,device_context) then
   begin
 
-    reportIdx:=0; //simple devices often use one endpoint (commonly $81) to output data reports
+    reportIdx:=0; //devices often use one endpoint (commonly $81) to output data reports
 
-{read report 5 times, then close the device}
-    for i:=0 to 4 do
-    begin
+{read report until keypressed, then closes the device}
+    repeat
       {interrupt reading   - for joystick or wiimote, or touchscreens, etc.
       NOTE: program execution is blocked until data is read from device!}
-      hidReportData[reportIdx].dataLen:=libusbhid_interrupt_read(device_context,$81{endpoint},{out}hidReportData[reportIdx].hid_data,128{report length, varies by device});
+      hidReportData[reportIdx].dataLen:=libusbhid_interrupt_read(device_context,$81{endpoint},{out}hidReportData[reportIdx].hid_data,128{report length, varies by device}, {timeout=}50);
 
-      PrintAndUpdateReport(reportIdx);
-    end;
+      PrintAndCompareReport(reportIdx);
+
+    until KeyPressed;
+
 		libusbhid_close_device(device_context);
 	end
   else WriteLn('unable to open device')
