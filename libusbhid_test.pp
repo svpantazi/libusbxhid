bc7422c2b9ec686af6cc2c7fe7fc611a2c8a2c1e
James Richters <james@productionautomation.net>
2019-08-16 16:01:37 -0400

    Use CRT instead of Keyboard, Change Device, Only Write report if return code is > 0
    Keyboard is not working on Windows 10

--- libusbhid_test.pp
+++ libusbhid_test.pp
@@ -18,7 +18,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
 
-  keyboard,
+  crt,
   sysutils,
 
   libusbhid,
@@ -29,13 +29,13 @@ begin
   if libusbhid_open_device(
         // $046D, $C216 {xbox gamepad }
         // $0000, $0001 {barcode scanner}
-	      // $24C0, $0003 {weather station}
-	      // $1784, $0001 {}
+        // $24C0, $0003 {weather station}
+        // $1784, $0001 {}
         // $056a, $00de {Wacom Bamboo}
-	      $04e7, $0050  {elo touch screen}
-//        $051d,$0002 {APC UPS}
-
-				,{instance=}1,device_context) then
+        // $04e7, $0050  {elo touch screen}
+        // $051d, $0002 {APC UPS}
+        $10CE, $EB93  {WHB04B-4 CNC Handwheel}
+         ,{instance=}1,device_context) then
   begin
 
     reportIdx:=0; //devices often use one endpoint (commonly $81) to output data reports
@@ -45,8 +45,8 @@ begin
       {interrupt reading   - for joystick or wiimote, or touchscreens, etc.
       NOTE: program execution is blocked until data is read from device!}
       hidReportData[reportIdx].dataLen:=libusbhid_interrupt_read(device_context,$81{endpoint},{out}hidReportData[reportIdx].hid_data,128{report length, varies by device}, {timeout=}50);
-
-      PrintAndCompareReport(reportIdx);
+      If hidReportData[reportIdx].dataLen > 0 Then
+         PrintAndCompareReport(reportIdx);
 
     until KeyPressed;
 
