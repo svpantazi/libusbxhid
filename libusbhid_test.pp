b05fa4b8db1e97fb09076c9bbaa42e93ffbe3901
svpantazi <32182348+svpantazi@users.noreply.github.com>
2019-08-28 18:28:12 -0400

    Add files via upload

--- libusbhid_test.pp
+++ libusbhid_test.pp
@@ -3,7 +3,7 @@ program libusbhid_test;
 S. V. Pantazi (svpantazi@gmail.com), 2013
 
 updated
-   08/13/2019
+	08/13/2019
   08/16/2019
 }
 
@@ -19,44 +19,45 @@ uses
   {$ENDIF}{$ENDIF}
 
   keyboard,
-  
   sysutils,
 
   libusbhid,
 
   hid_testing_utils;
 
+const
+	REPORT_DATA_IDX=0;
 begin
-keyboard.InitKeyboard();
+  keyboard.InitKeyboard();
 
   if libusbhid_open_device(
         // $046D, $C216 {xbox gamepad }
         // $0000, $0001 {barcode scanner}
-        // $24C0, $0003 {weather station}
-        // $1784, $0001 {}
+	      // $24C0, $0003 {weather station}
+	      // $1784, $0001 {}
         // $056a, $00de {Wacom Bamboo}
-        // $04e7, $0050  {elo touch screen}
-        // $051d, $0002 {APC UPS}
-        $10CE, $EB93  {WHB04B-4 CNC Handwheel}
-         ,{instance=}1,device_context) then
-    begin
-      Show_LibUSB_Messages:=False;
-      reportIdx:=0; //devices often use one endpoint (commonly $81) to output data reports
-      {read report until keypressed, then closes the device}
-      repeat
-        {interrupt reading   - for joystick or wiimote, or touchscreens, etc.
-        NOTE: program execution is blocked until data is read from device!}
-        hidReportData[reportIdx].dataLen:=libusbhid_interrupt_read(device_context,$81{endpoint},{out}hidReportData[reportIdx].hid_data,128{report length, varies by device}, {timeout=}50);
-        If hidReportData[reportIdx].dataLen > 0 Then
-           //PrintAndCompareReport(reportIdx,0);   //- Show all data of all reports
-           //PrintAndCompareReport(reportIdx,1);   //- Show Only Changed data of all reports
-           //PrintAndCompareReport(reportIdx,2);   //- Show all data only when report changed
-           PrintAndCompareReport(reportIdx,3);   //- Show Only Changed data only when report changed
-      until KeyPressed;
-      libusbhid_close_device(device_context);
-    end
-  else WriteLn('unable to open device');
-  keyboard.DoneKeyboard();
+	      $04e7, $0050  {elo touch screen}
+//        $051d,$0002 {APC UPS}
 
+				,{instance=}1,device_context) then
+  begin
+
+{read report until keypressed, then closes the device}
+    repeat
+      {interrupt reading   - for joystick or wiimote, or touchscreens, etc.
+      NOTE: program execution is blocked until data is read from device!}
+
+      {devices often use one endpoint (commonly $81) to output data reports}
+      libusbhid_interrupt_read(device_context,$81{endpoint},{out}hidReportData[REPORT_DATA_IDX].hid_data,128{report length, varies by device},hidReportData[REPORT_DATA_IDX].dataLen, {timeout=}1000);
+
+      PrintAndCompareReport(REPORT_DATA_IDX);
+
+    until KeyPressed;
+
+		libusbhid_close_device(device_context);
+	end
+  else WriteLn('unable to open device');
+
+  keyboard.DoneKeyboard();
 end.
 
